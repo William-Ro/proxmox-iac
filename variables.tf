936d1a583c2586776bb1bb109f788e9633df719a
William-Ro <william.r2409@gmail.com>
2025-07-25 14:48:43 -0600

    chore: update variables

--- variables.tf
+++ variables.tf
@@ -1,20 +1,17 @@
 variable "proxmox_url" {
-  description = "Proxmox API URL"
   type        = string
 }
 
 variable "proxmox_token_id" {
-  description = "Proxmox API token ID"
   type        = string
+  sensitive = true
 }
 
 variable "proxmox_token_secret" {
-  description = "Proxmox API token secret"
   type        = string
   sensitive   = true
 }
 
 variable "pm_node" {
-  description = "Proxmox node name"
   type        = string
 }
