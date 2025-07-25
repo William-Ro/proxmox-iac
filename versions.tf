81df850edfc0db28e63e13abfb9c2931bfc9e7c4
William-Ro <william.r2409@gmail.com>
2025-07-25 14:44:05 -0600

    chore: update to new provider version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 0.13.0"
 
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = ">= 2.9.11"
+      # Link https://github.com/Telmate/terraform-provider-proxmox
+      source = "telmate/proxmox"
+      version = "3.0.1-rc9"
     }
   }
 }
